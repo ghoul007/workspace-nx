14ef161f6a7edf7aaeea1ec46047a44dc3402790
ahmed <ahmed.elghoul@infor.com>
2020-06-23 00:26:21 +0100

    add ngrx in auth lib

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromAuth from './+state/auth.reducer';
+import { AuthEffects } from './+state/auth.effects';
+import { AuthFacade } from './+state/auth.facade';
 
 @NgModule({
   imports: [
@@ -9,6 +14,11 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+
+    StoreModule.forFeature(fromAuth.AUTH_FEATURE_KEY, fromAuth.reducer),
+
+    EffectsModule.forFeature([AuthEffects]),
   ],
+  providers: [AuthFacade],
 })
 export class AuthModule {}
